b16e578d2c1323c16a2fa3a5f524bf1a76ab258f
Daniil Glebov <dania.57.v@gmail.com>
2018-04-15 18:43:41 +0300

    Now my code is KISS

--- KISS.cpp
+++ KISS.cpp
@@ -9,42 +9,17 @@ using namespace std;
 int main()
 {
     int n, m, sum = 0, sum1 = 0, tek1 = 0, tek2 = 0, e = 0, w = 0;
-    char c;
-    string s, f;
-    getline(cin, s);
-    f = s[0];
-    for(int j = 1; j < s.size(); j++)
-        {
-            if (s[j] == ' ')
-            {
-                n = atoi(f.c_str());
-                f = "";
-            }
-            else
-            f += s[j];
-        }
-    m = atoi(f.c_str());
+    cin >> n >> m;
     vector< vector<int> > v1;
     vector<int> v2;
 
     int d;
     for(int i = 0; i < n; i++)
     {
-        getline(cin, s);
-        f = s[0];
-        for(int j = 1; j < s.size(); j++)
-        {
-            if (s[j] == ' ')
-            {
-                d = atoi(f.c_str());
-                v2.push_back(d);
-                f = "";
-            }
-            else
-            f += s[j];
+        for(int j = 0; j < m; j++) {
+            cin >> d;
+            v2.push_back(d);
         }
-        d = atoi(f.c_str());
-        v2.push_back(d);
         v1.push_back(v2);
         v2.clear();
     }
